79bc529cf99b630f01a986072bd32b9104bd9780
Yuto Harada <yuto.harada@polyscape.io>
2025-10-25 11:19:52 +0900

    init

---
+++ src/NodeGraph.Model/Class1.cs
@@ -0,0 +1,41 @@
+﻿namespace NodeGraph.Model;
+
+public interface IId
+{
+    Guid Value { get; }
+}
+
+public interface IWithId<out T>
+    where T : IId
+{
+    T Id { get; }
+}
+
+public record struct PortId(Guid Value) : IId;
+public record struct NodeId(Guid Value) : IId;
+
+public class Port : IWithId<PortId>
+{
+    public Port(Node parent)
+    {
+        Parent = parent;
+        Id = new PortId(Guid.NewGuid());
+    }
+    
+    public PortId Id { get; } 
+    public Node Parent { get; }
+    
+    public Port? ConnectedPort { get; set; }
+}
+
+public class Node : IWithId<NodeId>
+{
+    public NodeId Id { get; } = new(Guid.NewGuid());
+    public List<Port> InputPorts { get; } = [];
+    public List<Port> OutputPorts { get; } = [];
+}
+
+public class Graph
+{
+    public List<Node> Nodes { get; } = [];
+}
---
+++ test/NodeGraph.UnitTest/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using NodeGraph.Model;
+
+namespace NodeGraph.UnitTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        var graph = new Graph();
+        
+        var nodeA = new Node();
+        nodeA.OutputPorts.Add(new Port(nodeA));
+        var nodeAOutputPort = nodeA.OutputPorts[0];
+        
+        var nodeB = new Node();
+        nodeB.InputPorts.Add(new Port(nodeB));
+        var nodeBInputPort = nodeB.InputPorts[0];
+        nodeBInputPort.ConnectedPort = nodeAOutputPort;
+        nodeAOutputPort.ConnectedPort = nodeBInputPort;
+        
+        graph.Nodes.Add(nodeA);
+        graph.Nodes.Add(nodeB);
+    }
+}
