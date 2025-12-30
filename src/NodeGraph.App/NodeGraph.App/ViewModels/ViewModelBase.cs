3fffd15c434bc194abfa373330365a248d0159d2
roudo <roudo@example.com>
2025-12-30 10:43:38 +0900

    add cross platform

---
+++ src/NodeGraph.App/NodeGraph.App/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace NodeGraph.App.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ src/NodeGraph.App/NodeGraph.App/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace NodeGraph.App.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
